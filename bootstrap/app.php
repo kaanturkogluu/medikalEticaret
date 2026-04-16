4acd1d8b9c216f706ba1ad6f1e27d3edb7f2684a
Kaan TÜRKOĞLU <kaantrrkoglu@gmail.com>
2026-04-17 01:42:13 +0300

    sync orders scheduledan cikarildi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -23,11 +23,11 @@ return Application::configure(basePath: dirname(__DIR__))
         );
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
-            'user'  => \App\Http\Middleware\UserMiddleware::class,
+            'user' => \App\Http\Middleware\UserMiddleware::class,
         ]);
     })
     ->withSchedule(function (\Illuminate\Console\Scheduling\Schedule $schedule) {
-        $schedule->command('sync:trendyol-products')->everyThreeMinutes();
+
         $schedule->command('sync:orders')->everyThreeMinutes();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
