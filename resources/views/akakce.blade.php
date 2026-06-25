9871ec89fa01d239f19be4280ddacc420aea1e43
Kaan Türkoğlu <kaantrrkoglu@gmail.com>
2026-06-25 13:00:23 +0300

    akakce xml

--- resources/views/akakce.blade.php
+++ resources/views/akakce.blade.php
@@ -1,22 +1,33 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
-<products>
-    @foreach ($products as $product)
-    <product>
-        <productId>{{ $product->sku ?? $product->id }}</productId>
-        <name><![CDATA[{{ $product->name }}]]></name>
-        <barcode>{{ $product->barcode ?? '' }}</barcode>
-        <price>{{ number_format($product->price, 2, '.', '') }}</price>
-        <stock>{{ $product->stock }}</stock>
-        <category><![CDATA[{{ $product->category_name ?? ($product->category ? $product->category->name : '') }}]]></category>
-        
-        @php
-            $mainImage = $product->images->first();
-        @endphp
-        <imageUrl><![CDATA[{{ $mainImage ? asset('storage/' . $mainImage->image_path) : url('images/default-product.jpg') }}]]></imageUrl>
-        
-        <brand><![CDATA[{{ $product->brand_name ?? ($product->brand ? $product->brand->name : '') }}]]></brand>
-        
-        <url><![CDATA[{{ route('product.show', $product->slug ?? $product->id) }}]]></url>
-    </product>
-    @endforeach
+{!! '<?xml version="1.0" encoding="utf-8" standalone="yes"?>' !!}
+<products xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+@foreach ($products as $product)
+<product>
+<sku>{{ $product->sku ?? $product->id }}</sku>
+<name><![CDATA[{{ $product->name }}]]></name>
+<url>{{ route('product.show', $product->slug ?? $product->id) }}</url>
+@php
+    $images = $product->images;
+    $mainImage = $images->first();
+    $additionalImages = $images->skip(1)->take(5);
+@endphp
+<imgUrl>{{ $mainImage ? asset('storage/' . $mainImage->image_path) : url('images/default-product.jpg') }}</imgUrl>
+@if($additionalImages->count() > 0)
+<additionalimages>
+@foreach($additionalImages as $image)
+<imgUrl>{{ asset('storage/' . $image->image_path) }}</imgUrl>
+@endforeach
+</additionalimages>
+@endif
+<description><![CDATA[{!! $product->description !!}]]></description>
+<distributor></distributor>
+<price>{{ number_format($product->price, 2, '.', '') }}</price>
+<shipPrice>{{ $product->free_shipping ? '0.00' : '0.00' }}</shipPrice>
+<dayOfDelivery>3</dayOfDelivery>
+<expressDeliveryTime></expressDeliveryTime>
+<quantity>{{ $product->stock }}</quantity>
+<productBrand><![CDATA[{{ $product->brand_name ?? ($product->brand ? $product->brand->name : '') }}]]></productBrand>
+<productCategory><![CDATA[{{ $product->category_name ?? ($product->category ? $product->category->name : '') }}]]></productCategory>
+<barcode>{{ $product->barcode ?? '' }}</barcode>
+</product>
+@endforeach
 </products>
